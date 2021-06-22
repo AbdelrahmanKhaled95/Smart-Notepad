edb0b7407cbd763ddcee1616afb2caae9ab042fb
AbdelrahmanHussein95 <abdelrahman.hussein1995@gmail.com>
2021-06-22 10:31:37 +0200

    Add pod folder.

---
+++ Pods/Target Support Files/Pods-SmartNotepad-SmartNotepadUITests/Pods-SmartNotepad-SmartNotepadUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartNotepad_SmartNotepadUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartNotepad_SmartNotepadUITests
+@end
---
+++ Pods/Target Support Files/Pods-SmartNotepad/Pods-SmartNotepad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartNotepad : NSObject
+@end
+@implementation PodsDummy_Pods_SmartNotepad
+@end
