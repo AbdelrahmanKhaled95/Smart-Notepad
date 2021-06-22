9ba2fe8a4ad2c139b0a2f0c20d1222b0b2a64ca2
AbdelrahmanHussein95 <abdelrahman.hussein1995@gmail.com>
2021-06-22 09:28:45 +0200

    Add BaseViewController which contains all basic view functions.

---
+++ SmartNotepad/BaseViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BaseViewController.swift
+//  SmartNotepad
+//
+//  Created by Abdelrahman Hussein on 19/06/2021.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Looks for single or multiple taps.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillAppear() {
+        //Do something here
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func keyboardWillDisappear() {
+        //Do something here
+        self.view.gestureRecognizers?.removeAll()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func showAlert(_ message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
