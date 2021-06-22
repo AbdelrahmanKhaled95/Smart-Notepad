ac6a7ca7cb05202c0b3249599f28eb716b4bb6c7
AbdelrahmanHussein95 <abdelrahman.hussein1995@gmail.com>
2021-06-22 09:31:48 +0200

    Add string extension to change hex string to uicolor.

---
+++ SmartNotepad/Extensions/String+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  String+Extension.swift
+//  SmartNotepad
+//
+//  Created by Abdelrahman Hussein on 20/06/2021.
+//
+
+import UIKit
+
+extension String {
+    
+    //Convert From HEX string to UIColor
+    var toUIColor: UIColor {
+        var cString:String = self.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
